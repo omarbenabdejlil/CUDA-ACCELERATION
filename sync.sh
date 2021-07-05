288583a32233bdce18196a6e1d40eaf02780eda9
omarbenabdejlil <802.killer@gmail.com>
2021-07-05 11:00:33 +0100

    adding auto compile script !

---
+++ sync.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# auth : Omar benabdejlil
+echo -en "[+] - compiling each file ! .."
+sleep 2 
+
+for i in $(ls | grep -E "*.c" ); do nvcc $i|cut -d"." -f1 ; done
+echo -e "[!] - finishing , bye !"
+
+sleep 2 
+
