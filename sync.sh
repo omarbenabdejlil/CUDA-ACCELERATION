96b5e332f1b779eb92dfe6ae8fda10aeb9807a2e
omarbenabdejlil <802.killer@gmail.com>
2021-07-05 11:13:15 +0100

    modifing structure of repository

--- sync.sh
+++ sync.sh
@@ -3,7 +3,7 @@
 echo -en "[+] - compiling each file ! .."
 sleep 2 
 
-for i in $(ls | grep -E "*.c" ); do nvcc $i|cut -d"." -f1 ; done
+for i in $(ls | grep -E "*.c" ); do nvcc $i|cut -d"." -f1 ./bin/; done
 echo -e "[!] - finishing , bye !"
 
 sleep 2 
